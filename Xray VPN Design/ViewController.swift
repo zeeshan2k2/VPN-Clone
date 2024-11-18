b36a61ea6a0be29365030efe7d183d854ed7c142
Zeeshan Waheed <zeeshan@synapse-consulting.com.pk>
2024-11-18 17:36:40 +0500

    Initial Commit

---
+++ Xray VPN Design/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xray VPN Design
+//
+//  Created by Zeeshan Waheed on 18/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
