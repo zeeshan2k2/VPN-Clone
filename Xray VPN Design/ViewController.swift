07e754634176f5be925e319f73b6234dd79c14c4
Zeeshan Waheed <zeeshan@synapse-consulting.com.pk>
2024-11-18 19:35:31 +0500

    first screen done

--- Xray VPN Design/ViewController.swift
+++ Xray VPN Design/ViewController.swift
@@ -9,15 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    @IBOutlet weak var memberBtn: UIButton!
+    @IBOutlet weak var supportBtn: UIButton!
+    @IBOutlet weak var ipBtn: UIButton!
+    @IBOutlet weak var connectBtn: UIButton!
+    
     @IBOutlet weak var controlsBG: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        controlsBG.layer.cornerRadius = 10
+        controlsBG.layer.cornerRadius = 15
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func menuBtnclicked(_ sender: Any) {
+        print("menu button clicked")
+    }
+    
+    @IBAction func memberBtnclicked(_ sender: Any) {
+        print("member button clicked")
+    }
+    
+    @IBAction func supportBtnclicked(_ sender: Any) {
+        print("support button clicked")
+    }
+    
+    @IBAction func ipBtnclicked(_ sender: Any) {
+        print("ip button button clicked")
+    }
+    
+    @IBAction func connectBtnclicked(_ sender: Any) {
+        print("connect button clicked")
+    }
+    
+    @IBAction func fastestLocationBtnclicked(_ sender: Any) {
+        print("fastest location button clicked")
+    }
+    
+    @IBAction func autoModeBtnclicked(_ sender: Any) {
+        print("automode button clicked")
+    }
+    
 }
 
